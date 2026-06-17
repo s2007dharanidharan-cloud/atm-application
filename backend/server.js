d0aa791748936230e1b6ac8958993d980cab4e30
Dharanidharan <s2007dharanidharan@gmail.com>
2026-06-17 11:38:09 +0530

    Fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
